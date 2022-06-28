092e3cfae14f0438c5965177727ea73de7e84eb2
aoffret <ashleyoffret1@gmail.com>
2022-06-28 14:46:17 -0600

    successfully got fetch working in javascript

---
+++ fetch.js
@@ -0,0 +1,32 @@
+const url = "trainings.json";
+const outputElement = document.getElementById('output');
+let results = null;
+async function getPokemon(url) {
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    console.log('here');
+    const data = await response.json();
+
+    //Loop and print each part of the array.
+    for(let i = 0; i < data.options.length; i++){
+      console.log(data.options[i].name)
+    }
+
+    //data.options.forEach(console.log("Made it!"))
+
+    //console.log(data.options[0])
+    //doStuff(data);
+  }
+}
+function doStuff(data) {
+    const html = `<h2>${data.name}</h2>`;
+    //Use backticks for above item to put in object info
+    outputElement.innerHTML = html;
+}
+getPokemon(url);
+console.log("second: ", results);
+
+//Find fetch file that goes through each option
