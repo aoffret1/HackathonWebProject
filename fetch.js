bdea8622021494d39fdb3fd955dd16c80623df53
thunderbioink <allealle0303@gmail.com>
2022-06-29 11:16:16 -0600

    scratch

--- fetch.js
+++ fetch.js
@@ -98,12 +98,29 @@ function reset() {
   element.innerHTML = '';
 }
 
+// Search training bar
 
 function searchFunction(){
   //Declare variables to search table:
   var input, filter, table, tr, td, i, txtValue;
-  
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");  
 }
+
+// // Loop through trainings:
+// for (i = 0; i < tr.length; i++) {
+//   td = tr[i].getElementsByTagName("td")[0];
+//   if (td) {
+//     txtValue = td.textContent || td.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       tr[i].style.display = "";
+//     } else {
+//       tr[i].style.display = "none";
+//     }
+//   }
+// }
 
 getJSON(url);
 
